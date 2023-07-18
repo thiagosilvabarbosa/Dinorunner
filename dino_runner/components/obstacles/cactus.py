f0e0cbe3c2c01be198dbcc7322df7433cbbc3e3b
Thiago Barbosa <contatobarbosathiago@gmail.com>
2023-07-18 20:04:48 -0300

    obstacle adjustments
    ajuste no eixo y dos cactus

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -7,4 +7,4 @@ class Cactus(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = 325
+        self.rect.y = 320
