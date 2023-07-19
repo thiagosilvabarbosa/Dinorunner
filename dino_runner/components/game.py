954cc7b99c90efcbb750ee6a9adf0d26404167fd
Thiago Barbosa <contatobarbosathiago@gmail.com>
2023-07-19 20:02:34 -0300

    add xpos

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -94,6 +94,7 @@ class Game:
         if self.x_pos_bg <= -image_width:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
+        self.x_pos_bg -= self.game_speed
 
     def draw_score(self):
         font = pygame.font.Font(FONT_STYLE, 22)
